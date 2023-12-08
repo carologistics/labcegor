d884919da40d71419fde147b50625f87b2ef1eda
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2023-12-08 10:00:09 +0100

    fix refbox launch script

--- launch/refbox.py
+++ launch/refbox.py
@@ -6,6 +6,7 @@ import argparse
 import rclpy
 from rclpy.node import Node
 
+
 class RefboxNode(Node):
     def __init__(self):
         super().__init__('refbox_node')
@@ -17,18 +18,18 @@ class RefboxNode(Node):
             ]
         )
 
-
     def get_terminal_command(self, terminal, shell, commands, tab_names=None):
         if terminal == 'tmux':
             term_command = f"{terminal} new-session -s refbox -d; {terminal} set-window-option -g remain-on-exit on"
             for i, cmd in enumerate(commands):
                 tab_name = f"-n \"{tab_names[i]}\"" if tab_names else ""
-                term_command += f"; {terminal} new-window {tab_name} '{shell} -i -c \"{cmd}\"'"
+                term_command += f"; {terminal} new-window {tab_name} '{shell} -i -c \"{cmd}\"; {terminal} set-window-option -g remain-on-exit on'"
         else:  # Assuming gnome-terminal as the default
-            term_command = f"{terminal} --tab"
+            term_command = f"{terminal} "
             for i, cmd in enumerate(commands):
                 tab_name = f"--title=\"{tab_names[i]}\"" if tab_names else ""
-                term_command += f" --tab {tab_name} -e '{shell} -i -c \"{cmd}\"'"
+                term_command += f" --tab {tab_name} -e '{shell} -i -c \"{cmd} ; echo -n \\\"Press ENTER to exit...\\\" && read -n 1 -s -r\"'"
+
         return term_command
 
 
@@ -41,19 +42,21 @@ def main():
         terminal = node.get_parameter('terminal').value
         shell = node.get_parameter('shell').value
         tab_names = ['refbox', 'refbox-frontend']
-        print(terminal, shell)
-        print(terminal, shell)
-        print(terminal, shell)
+        commands = [
+            f"cd {os.environ['LLSF_REFBOX_DIR']}/bin && ./llsf-refbox --cfg-mps mps/mockup_mps.yaml",
+            f"cd {os.environ['RCLL_REFBOX_FRONTEND_DIR']} && npm run serve"
+        ]
 
         # Construct the terminal command using the parameters
-        term_command = node.get_terminal_command(terminal, shell, tab_names)
+        term_command = node.get_terminal_command(
+            terminal, shell, commands, tab_names)
         subprocess.run(term_command, shell=True)
-        # Rest of your code...
+        print(term_command)
 
     finally:
         node.destroy_node()
         rclpy.shutdown()
 
+
 if __name__ == "__main__":
     main()
-
